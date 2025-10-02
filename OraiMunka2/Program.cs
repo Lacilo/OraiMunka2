59862e825d88ca54bf82f7a28a19420cf8ad24d4
Lacilo <szaszfail@kkszki.hu>
2025-10-02 09:49:55 +0200

    9.feladatig kész

--- OraiMunka2/Program.cs
+++ OraiMunka2/Program.cs
@@ -10,7 +10,102 @@ namespace OraiMunka2
     {
         static void Main(string[] args)
         {
+            /*
+
             // ELSŐ FEADAT
+            Console.WriteLine("Hello World!");
+
+            // 2
+            Console.Write("Adja meg a nevét --> ");
+            string nev = Console.ReadLine();
+            Console.WriteLine($"Üdvözlöm {nev}!");
+
+            // 3
+            Console.Write("Adja meg a számot --> ");
+            int szam = int.Parse(Console.ReadLine());
+            Console.WriteLine($"A szám kétszerese {szam * 2}");
+
+            // 4
+            Console.Write("első szám --> ");
+            int szam1 = int.Parse(Console.ReadLine());
+            Console.Write("második szám --> ");
+            int szam2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"összeg: {szam1 + szam2}");
+            Console.WriteLine($"külömbség: {szam1 - szam2}");
+            Console.WriteLine($"szorzat: {szam1 * szam2}");
+
+            if (szam2 != 0)
+            {
+                Console.WriteLine($"hányados: {szam1 / szam2}");
+            }
+            
+            // 5
+            int nSzam1 = int.Parse(Console.ReadLine());
+            int nSzam2 = int.Parse(Console.ReadLine());
+
+            if (nSzam1 > nSzam2)
+            {
+                Console.WriteLine(nSzam1);
+            }else if(szam2 > szam1)
+            {
+                Console.WriteLine(szam2);
+            }
+            else
+            {
+                Console.WriteLine("ugyan akkora");
+            }
+
+            // 6
+            int[] szamok = new int[3];
+
+            for(int i = 0; i < 3; i++)
+            {
+                szamok[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(szamok.Min());
+
+            
+
+            // 8
+            double mSzam1 = int.Parse(Console.ReadLine());
+            double mSzam2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((mSzam1 + mSzam2) / 2);
+
+            Console.WriteLine(Math.Sqrt(mSzam1 * mSzam2));
+            
+            
+
+            // 7
+
+            double a = 0;
+            double b = 0;
+            double c = 0;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+
+            if (a + b > c && b + c > a)
+            {
+                Console.WriteLine("szerkeszthető");    
+            }
+            else
+            {
+                Console.WriteLine("nem szerkeszthető");
+            }
+
+            */
+
+
+            // 9
+
+
+
+
+
         }
     }
 }
